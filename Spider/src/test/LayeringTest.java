c8437d703798af22eda420511353ff6089fe5f7c
Joe-Joe-Joe <qqiaoyu@163.com>
2017-03-17 12:53:06 +0800

    Add Layering test

---
+++ Spider/src/test/LayeringTest.java
@@ -0,0 +1,18 @@
+/**
+ * @date 2017年3月17日
+ * @version 1.0
+ */
+package test;
+
+public class LayeringTest {
+
+	/**
+	 * layering test
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
